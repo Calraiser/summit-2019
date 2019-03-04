03ffebd2dc523ea065adf12e07f49711fef071f8
carlos.alvarez <carlos.alvarez@28apo5491.hi.inet>
2019-03-04 17:30:37 +0100

    Responsive & Fake Modal

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,5 +1,20 @@
+import './components/fakeDemo.js'
+
 require('script-loader!jquery');
+require('script-loader!bootstrap');
 
 $(document).ready(function() {
 
+  var nav = $('.navigation a');
+
+  nav.on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        target.prop("checked", "checked");
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 30
+        }, 1000);
+      }
+  });
 });
